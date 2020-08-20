d19788ab9bbe4b8182bf69d22090400897ab99b5
PinskyPhilipp <39645492+PinskyPhilipp@users.noreply.github.com>
2020-08-20 09:26:50 +0300

    Урок №2
    Выполнил задачу №1

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const firstRow = prompt('Введите предложение например "мама мыла раму"');
+const secondRow = prompt('Введите предложение например "собака друг человека"');
+
+let firstRowCount = 0;
+let secondRowCount = 0;
+
+function getRow(firstRow, secondRow) {
+    for (let i = 0 ; i < firstRow.length; i++){
+    if(firstRow.charAt(i) == 'а'){
+       firstRowCount++;
+    }
+};
+
+for (let i = 0 ; i < secondRow.length; i++){
+    if(secondRow.charAt(i) == 'а'){
+       secondRowCount++;
+    }
+};
+
+if(firstRowCount > secondRowCount ){
+    alert('В этом предложении ' + '"' + firstRow + '"' + ' больше буквы "а"');
+}else if(firstRowCount == secondRowCount){
+    alert('Тут нет ни одной буквы "а"')
+}else {
+    alert('В этом предложении ' + '"' + secondRow + '"' + ' больше буквы "а"');
+}
+}
+
+getRow(firstRow, secondRow)
